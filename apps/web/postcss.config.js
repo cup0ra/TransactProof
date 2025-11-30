c7fb86da00378b4d0850e5005f433d20f304619e
Siarhei_Baranenkau <siarhei_baranenkau@epam.com>
2025-11-30 15:53:19 +0400

    add

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,18 +1,6 @@
-const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
-};
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.cssnano = {
-    preset: ['default', {
-      discardComments: {
-        removeAll: true,
-      },
-    }],
-  };
-}
-
 module.exports = {
-  plugins,
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
